13cd8000de950869edca5eeb8e57627680fca660
Lucie Saad <luciesaad@outlook.com>
2019-08-28 15:08:23 +0200

    added Start structure

---
+++ react_project_intranet/src/containers/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Start} from './Start.js'
+import {Nav} from "../Components/Nav";
+
+function App() {
+  return (
+   <div>
+       <p> hej </p>
+        <Start/>
+       <Nav/>
+
+   </div>
+  );
+}
+
+export default App;
