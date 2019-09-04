f16c7f686cd68cc49aba8ef57353d67990f7efc4
Lucie Saad <luciesaad@outlook.com>
2019-09-04 16:00:52 +0200

    set up server, dummy data communication w/ frontend

--- react_project_intranet/src/components/app/intranet/app-intranet.js
+++ react_project_intranet/src/components/app/intranet/app-intranet.js
@@ -4,12 +4,16 @@ import {AppIntranetNavbar} from "./navbar/app-intranet-navbar";
 
 import MontisoriLogo from '../../../assets/images/logo_montessorivillorna.png'
 
-const AppIntranet = () => {
+const AppIntranet = (props) => {
     return (
         <div>
             <AppIntranetNavbar/>
             <img src={MontisoriLogo} alt="Logotype for Montisori school"/>
             <h1>Welcome</h1>
+            <ul>
+                {props.showUsers.map(value => (<li key={value}> {value}  </li>))}
+                {props.showPass.map(value => (<li key={value}> {value}  </li>))}
+            </ul>
         </div>
     )
 };
--- react_project_intranet/src/index.js
+++ react_project_intranet/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import App from './Components/app';
 import './index.css';
 //import * as serviceWorker from './serviceWorker';
 
