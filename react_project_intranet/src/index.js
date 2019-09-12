902d4fd89a686d1526b5efcd6824653c3e370a0c
Lucie Saad <lucie.saad@outlook.com>
2019-09-12 13:06:19 +0200

    components with small letter c

--- react_project_intranet/src/index.js
+++ react_project_intranet/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/app';
+import App from './components/app';
 import './index.css';
 
 import 'tachyons';
 import '@material-ui/core';
 
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import LogIn from './Components/app/logIn'
+import LogIn from './components/app/logIn'
 
 ReactDOM.render( <Router>
     <div className="app" >
