64d4be57eaec568f4f727b2cdbd2ab71a5860501
Lars Strömberg <stromberg.lars@gmail.com>
2019-09-17 21:47:20 +0200

    correction redux

--- react_project_intranet/src/components/app/intranet/left/gallery/app-intranet-left-gallery.js
+++ react_project_intranet/src/components/app/intranet/left/gallery/app-intranet-left-gallery.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const AppIntranetLeftGallery = () => {
+    let sec = new Date(). getSeconds(); //Current Seconds.
     return (
         <div className='bg-light-blue br2 grow pa3 ma2 bw2 shadow-5'>
-            <h1>Gallery</h1>
+            <h1>Bildgalleri</h1>
+            <img alt='robots' src={`https://robohash.org/${sec}?size=200x200`} />
         </div>
     )
 };
--- react_project_intranet/src/components/app/redux/actions.js
+++ react_project_intranet/src/components/app/redux/actions.js
@@ -1,6 +1,35 @@
-import {CHANGE_SEARCH_FIELD} from "./constants";
+import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_EMPLOYEE_PENDING,
+    REQUEST_EMPLOYEE_SUCCESS,
+    REQUEST_EMPLOYEE_FAILED,
+    LOGGED_IN_AS_ADMIN
+} from "./constants";
 
+// this is an object and redux-thunk is not needed but can handle both and act accordingly
 export const setSearchField = (text) => ({          // ({ automatic return =) })
     type: CHANGE_SEARCH_FIELD,
     payload: text
 });
+
+// This returns a function that we need redux-thunk to handle
+// This is a function that returns a function
+// The first function provide the "dispatch function" to the second function
+export const requestEmployees = () => (dispatch) => {   // Higher order function
+    dispatch({type: REQUEST_EMPLOYEE_PENDING});
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => dispatch({type: REQUEST_EMPLOYEE_SUCCESS, payload: data}))
+        .catch(error => dispatch({ type: REQUEST_EMPLOYEE_FAILED, payload: error}))
+};
+
+export const setAdminStatus = (text) => ({
+    type: LOGGED_IN_AS_ADMIN,
+    payload: text
+});
+
+/*
+export const setAdminStatusFalse = ({
+    type: LOGGED_IN_AS_ADMIN,
+    payload: false
+});*/
